ed87b7f83f1146de7dd19ac7abb837f3759b0b9d
arielbulacio <129123665+arielbulacio@users.noreply.github.com>
2023-05-06 20:40:43 -0300

    Agrego Clase genero

--- src/main/java/UTNFRGP/tp3_grupo_3/entidad/Genero.java
+++ src/main/java/UTNFRGP/tp3_grupo_3/entidad/Genero.java
@@ -1,4 +1,50 @@
 package UTNFRGP.tp3_grupo_3.entidad;
 
-public class Genero {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="Genero")
+public class Genero implements Serializable {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO) 
+	private int id;
+	
+	@Column
+	private String descripción;
+	
+	public Genero()
+	{
+		
+	}
+
+	
+	public String getDescripción() {
+		return descripción;
+	}
+
+	public void setDescripción(String descripción) {
+		this.descripción = descripción;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Genero [id=" + id + ", descripción=" + descripción + "]";
+	}
+	
+	
+	
 }
+
+
